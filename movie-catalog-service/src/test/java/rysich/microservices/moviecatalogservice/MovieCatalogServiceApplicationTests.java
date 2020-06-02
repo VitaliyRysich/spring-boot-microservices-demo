fd7a7de5fc59fdf397842d393a4be87cbdfd12f6
Vitalii Rysich <vitalii.rysich@efl.com.pl>
2020-06-02 20:20:30 +0200

    init commit

---
+++ movie-catalog-service/src/test/java/rysich/microservices/moviecatalogservice/MovieCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package rysich.microservices.moviecatalogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
