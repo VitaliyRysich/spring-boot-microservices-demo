425556c3b01674f2fb4ac8e82fe44eaa8351c09c
Vitalii Rysich <vitalii.rysich@efl.com.pl>
2020-06-02 22:59:19 +0200

    Added Ratings Data Service API

---
+++ ratings-data-service/src/main/java/rysich/microservices/ratingsdataservice/resources/RatingResource.java
@@ -0,0 +1,16 @@
+package rysich.microservices.ratingsdataservice.resources;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import rysich.microservices.ratingsdataservice.models.Rating;
+
+@RestController
+@RequestMapping("/ratingsdata")
+public class RatingResource {
+
+    @RequestMapping("/{movieId}")
+    public Rating getRating(@PathVariable("movieId") String movieId){
+        return new Rating(movieId, 4);
+    }
+}
