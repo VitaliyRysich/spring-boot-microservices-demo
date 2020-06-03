503af82ca1f9529704dab36966eb8c67710298da
Vitalii Rysich <vitalii.rysich@efl.com.pl>
2020-06-03 17:04:33 +0200

    Calling Ratings Data API

--- movie-catalog-service/src/main/java/rysich/microservices/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/rysich/microservices/moviecatalogservice/resources/MovieCatalogResource.java
@@ -1,7 +1,6 @@
 package rysich.microservices.moviecatalogservice.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,10 +8,8 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 import rysich.microservices.moviecatalogservice.models.CatalogItem;
 import rysich.microservices.moviecatalogservice.models.Movie;
-import rysich.microservices.moviecatalogservice.models.Rating;
+import rysich.microservices.moviecatalogservice.models.UserRating;
 
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -29,20 +26,10 @@ public class MovieCatalogResource {
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
 
-        List<Rating> ratings = Arrays.asList(
-                new Rating("1234", 4),
-                new Rating("2344", 3)
-        );
+        UserRating ratings = restTemplate.getForObject("http://localhost:8083/ratingsdata/user/" + userId, UserRating.class);
 
-        return ratings.stream().map(rating -> {
-            //Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
-
-            Movie movie = webClientBuilder.build()
-                    .get()
-                    .uri("http://localhost:8082/movies/" + rating.getMovieId())
-                    .retrieve()
-                    .bodyToMono(Movie.class)
-                    .block();
+        return ratings.getUserRating().stream().map(rating -> {
+            Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
 
 
             return new CatalogItem(movie.getName(), "Test", rating.getRating());
@@ -51,3 +38,13 @@ public class MovieCatalogResource {
 
     }
 }
+
+/*
+Movie movie = webClientBuilder.build()
+        .get()
+        .uri("http://localhost:8082/movies/" + rating.getMovieId())
+        .retrieve()
+        .bodyToMono(Movie.class)
+        .block();
+
+ */
--- ratings-data-service/src/main/java/rysich/microservices/ratingsdataservice/resources/RatingResource.java
+++ ratings-data-service/src/main/java/rysich/microservices/ratingsdataservice/resources/RatingResource.java
@@ -4,6 +4,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import rysich.microservices.ratingsdataservice.models.Rating;
+import rysich.microservices.ratingsdataservice.models.UserRating;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/ratingsdata")
@@ -12,5 +16,16 @@ public class RatingResource {
     @RequestMapping("/{movieId}")
     public Rating getRating(@PathVariable("movieId") String movieId){
         return new Rating(movieId, 4);
+    }
+
+    @RequestMapping("/user/{userId}")
+    public UserRating getUserRating(@PathVariable("userId") String userId){
+        List<Rating> ratings = Arrays.asList(
+                new Rating("1234", 4),
+                new Rating("2344", 3)
+        );
+        UserRating userRating = new UserRating();
+        userRating.setUserRating(ratings);
+        return userRating;
     }
 }
